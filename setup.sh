05873f6ca7efa48a0d596d71209358b406925521
crixsz <xenonzox@gmail.com>
2024-11-20 10:36:13 +0800

    clearing after docker setup

--- setup.sh
+++ setup.sh
@@ -83,6 +83,8 @@ setup_cf_warp(){
   clear
   echo "[Docker Setup]"
   wget https://raw.githubusercontent.com/crixsz/DockerInstall/main/docker-install.sh && chmod +x docker-install.sh && ./docker-install.sh
+  clear
+  sleep 2
   echo "[CF Warp Setup]"
   ## moving to https://github.com/aleskxyz/warp-svc
   docker run --restart always -d --name=warp -e FAMILIES_MODE=off -p 127.0.0.1:1080:1080 -v ${PWD}/warp:/var/lib/cloudflare-warp ghcr.io/aleskxyz/warp-svc:latest
