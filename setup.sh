f5cf944812d9cc64b1844906948d0b3a9551711b
crixsz <xenonzox@gmail.com>
2025-02-17 10:08:07 +0800

    update warp setup script to use absolute path for volume mount

--- setup.sh
+++ setup.sh
@@ -91,7 +91,7 @@ setup_cf_warp(){
   sleep 2
   echo "[CF Warp Setup]"
   ## moving to https://github.com/aleskxyz/warp-svc
-  docker run --restart always -d --name=warp -e FAMILIES_MODE=off -p 127.0.0.1:1080:1080 -v ${PWD}/warp:/var/lib/cloudflare-warp ghcr.io/aleskxyz/warp-svc:latest
+  docker run --restart always -d --name=warp -e FAMILIES_MODE=off -p 127.0.0.1:1080:1080 -v /usr/local/warp:/var/lib/cloudflare-warp ghcr.io/aleskxyz/warp-svc:latest
   docker ps
   sleep 5
   ## bash <(curl -fsSL git.io/warp.sh) install (warp-cli currently causes SSH to disconnect)
